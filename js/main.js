35d23bf5bd9667542816cf076c545ccc09ca57a7
Kasper <mirzohid.rahmatullaev@gmail.com>
2021-09-02 01:09:35 +0500

    create: animation for whole page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
+	// Анимация AOS
+
+	AOS.init();
+
+	AOS.init({
+
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 50, // offset (in px) from the original trigger point
+		delay: 200, // values from 0 to 3000, with step 50ms
+		duration: 600, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		anchorPlacement: 'top-center', // defines which position of the element regarding to window should trigger the animation
+	});
+	// Hamburger
 	const hamburger = document.querySelector('.header-burger'),
 		headerItem = document.querySelectorAll('.header-burger__item'),
 		burgerList = document.querySelector('.header-nav__list'),
